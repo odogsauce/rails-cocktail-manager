3c0118f6fdba99a6418b836f32ae214996a8acdd
thibault rahou <trahou33@gmail.com>
2020-10-05 16:27:04 +0200

    design

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -25,8 +25,9 @@ class CocktailsController < ApplicationController
   end
 
   def destroy
+    @cocktail = Cocktail.find(params[:id])
     @cocktail.destroy
-    redirect_to cocktails_path
+    redirect_to root_pathth
   end
 
   private
